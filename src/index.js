6e95bbb3cd555e5bbe14ac6f0c4e4d68f675c0fb
VladimirSt13 <Vladimir.Stognij@gmail.com>
2022-08-22 14:12:27 +0300

    ...

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  input: document.querySelector('#search-box'),
+};
+
+refs.input.addEventListener('input', debounce(onChangeInput, DEBOUNCE_DELAY));
+
+function onChangeInput(evt) {
+  console.log('debouced', evt.target.value);
+}
+
+function fetchCountries(name) {}
